6b50323a2c083d9941992b50521d8ed8136f61d1
Rahim <rahimssenabulya82@gmail.com>
2025-04-19 17:33:59 +0300

    fix pesapal issues

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ async function getPesapalToken(){
     };
     try {
         const response = await
-        post(`${PESAPAL_URL}/Auth/RequestToken`, credentials,{
+        axios.post(`${PESAPAL_URL}/Auth/RequestToken`, credentials,{
             headers: {
                 'Content-Type':
                 'application/json'
